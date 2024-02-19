556e86cd433fe40e09094049dc5c8d5da83af80d
Johannes Deml <public@deml.io>
2024-02-19 23:30:01 +0100

    Add support for github code blocks
    They use an `data-code-text` attribute to store the text of the code lines, this needs different processing

--- chrome/content/scripts/content.js
+++ chrome/content/scripts/content.js
@@ -34,6 +34,33 @@ function runGuidReplacer() {
   var matchCount = 0;
   var nodeCount = 0;
 
+  // Collect all text nodes with possible GUID values (default)
+  function traverseTextNodes(element) {
+    var treeWalker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
+
+    // Cache all nodes to be able to add nodes while traversing
+    var textNodes = [];
+    while (treeWalker.nextNode()) {
+      var node = treeWalker.currentNode;
+
+      // Skip nodes that are too short to contain a GUID
+      if (!node.nodeValue || node.nodeValue.length < 32) {
+        continue;
+      }
+
+      // skip script nodes
+      if (node.parentNode.nodeName === "SCRIPT") {
+        continue;
+      }
+
+      textNodes.push(treeWalker.currentNode);
+    }
+
+    textNodes.forEach(function(node) {
+      processTextNode(node);
+    });
+  }
+
   // Function to process a text node and add labels to GUIDs
   function processTextNode(node) {
     var originalContent = node.nodeValue;
@@ -44,10 +71,10 @@ function runGuidReplacer() {
 
       var replacement = guidLookup[match];
       var tagText = replacement ? replacement.fileName : "No matching GUID found";
+      matchCount++;
       return match + '<span class="guidResolverTag">[' + tagText + ']</span>';
     });
     if (originalContent !== modifiedContent) {
-      matchCount++;
       node.nodeValue = '';
       var span = document.createElement('span');
       span.innerHTML = modifiedContent;
@@ -56,25 +83,64 @@ function runGuidReplacer() {
     nodeCount++;
   }
 
-  // Recursive function to traverse all text nodes in an element
-  function traverseTextNodes(element) {
-    var treeWalker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
 
-    // Cache all nodes to be able to add nodes while traversing
-    var textNodes = [];
-    while (treeWalker.nextNode()) {
-      var node = treeWalker.currentNode;
-      // Skip nodes that are too short to contain a GUID
-      if (!node.nodeValue || node.nodeValue.length < 32) {
-        continue;
+  // collect all special github elements with data-code-text attribute
+  function traverseDataCodeTextElements(element) {
+    var treeWalker = document.createTreeWalker(element, NodeFilter.SHOW_ELEMENT, {
+      acceptNode: function(node) {
+        if (node.hasAttribute('data-code-text') && node.getAttribute('data-code-text').length >= 32) {
+          return NodeFilter.FILTER_ACCEPT;
+        }
+        return NodeFilter.FILTER_SKIP;
       }
-      textNodes.push(treeWalker.currentNode);
+    }, false);
+
+    // Cache all elements to be able to add elements while traversing
+    var dataCodeTextElements = [];
+    while (treeWalker.nextNode()) {
+      var element = treeWalker.currentNode;
+      dataCodeTextElements.push(element);
     }
 
-    textNodes.forEach(function(node) {
-      processTextNode(node);
+    dataCodeTextElements.forEach(function(element) {
+      processElementWithCodeTextAttribute(element);
     });
   }
+
+  // Function to process an element with data-code-text attribute
+  // Tag elements need to be added in between the attributes to give them a special style
+  function processElementWithCodeTextAttribute(element) {
+    var originalContent = element.getAttribute('data-code-text');
+    var matches = originalContent.match(guidRegex);
+    var currentIndex = 0;
+    if (matches) {
+      for (var i = 0; i < matches.length; i++) {
+        var match = matches[i];
+        if (match === invalidGuid) {
+          continue;
+        }
+
+        var replacement = guidLookup[match];
+        var tagText = replacement ? replacement.fileName : "No matching GUID found";
+
+        matchCount++;
+        var index = originalContent.indexOf(match, currentIndex);
+        var upToMatch = originalContent.substring(currentIndex, index + match.length);
+        var afterMatch = originalContent.substring(index + match.length);
+        currentIndex = index + match.length;
+
+        var upToMatchElement = element.cloneNode(true);
+        upToMatchElement.setAttribute('data-code-text', upToMatch);
+        element.parentNode.insertBefore(upToMatchElement, element);
+        var tagElement = document.createElement('span');
+        tagElement.innerHTML = '[' + tagText + ']';
+        tagElement.classList.add('guidResolverTag');
+        upToMatchElement.parentNode.insertBefore(tagElement, upToMatchElement.nextSibling);
+        element.setAttribute('data-code-text', afterMatch);
+      }
+    }
+  }
+
 
   // measure time
   var startTime = performance.now();
@@ -87,6 +153,8 @@ function runGuidReplacer() {
 
   // Traverse all text nodes in the document body
   traverseTextNodes(document.body);
+  traverseDataCodeTextElements(document.body);
+
   console.log("Found " + matchCount + " matches" + " in " + nodeCount + " nodes (duration: " + (performance.now() - startTime) + " ms)");
 }
 
