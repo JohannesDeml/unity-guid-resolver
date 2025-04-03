4b9efcdcd30ee1938d4610df6796d351fa2aa642
Johannes Deml <public@deml.io>
2025-04-03 13:55:06 +0200

    Change improve icons
    Svg codes are from Tabler

--- chrome/content/popup/popup.js
+++ chrome/content/popup/popup.js
@@ -162,7 +162,13 @@ function updateFileList() {
           <div>${fileData.fileName}</div>
           <div style="font-size: 0.8em; color: #666;">${fileData.creationDate}</div>
           <div style="font-size: 0.8em;">${Object.keys(fileData.mapping).length} entries</div>
-          <button class="delete-btn" data-file-id="${fileId}">x</button>
+          <button class="delete-btn" data-file-id="${fileId}">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+              <path d="M18 6l-12 12" />
+              <path d="M6 6l12 12" />
+            </svg>
+          </button>
         </div>
       `;
     }).join('');
